3d5c78df6cc575a542f5d92a782a1b80686654d9
Alejandro Bermúdez <alejo0395@gmail.com>
2021-05-21 16:56:24 -0500

    test

--- src/screens/Dashboard/screens/Home/index.tsx
+++ src/screens/Dashboard/screens/Home/index.tsx
@@ -40,11 +40,16 @@ function Home() {
     }
   });
 
+  const handleClick = () => {
+    console.log('here');
+  };
+
   return (
     <div className={styles.app}>
       <header className={styles.appHeader}>
         <img src={logo} className={styles.appLogo} alt="logo" />
-        <p>TEST</p>
+        <p>TEST 1 - testing changes</p>
+        <button type="button">test</button>
         <p className={styles.text}>{t('Home:loggedIn')}</p>
         <p className={styles.text}>{t('Home:techIs', { tech })}</p>
         <form className="column center m-bottom-10" onSubmit={onSubmit}>
